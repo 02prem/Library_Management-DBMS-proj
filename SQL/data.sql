2ab6f9d46e53b7f6af5c2940ec80a7731a6a75ab
02prem <79003626+02prem@users.noreply.github.com>
2023-07-19 11:33:09 +0530

    add data to the tables

---
+++ SQL/data.sql
@@ -0,0 +1,8 @@
+insert into student(student_roll_number,student_full_name,book_id)
+VALUES('IMT2020044','Prem Shah',NULL);
+
+insert into book(book_id,book_name,book_author,student_rno,admin_roll_number)
+VALUES('123124','Harry Potter',' J K R',NULL,NULL);
+
+insert into library_manager(admin_name,password_admin)
+VALUES('Manager','123');
